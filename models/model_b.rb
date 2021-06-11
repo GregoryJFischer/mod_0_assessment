fffcfde56325f60f51c7a19ce2e37d299f72f9b5
Gregory Fischer <gwhoisj@gmail.com>
2021-06-10 22:49:31 -0400

    Add myself

---
+++ models/model_b.rb
@@ -0,0 +1,12 @@
+sowftware_engineer = {
+  "Name" => "Greg"
+  "Pronouns" => "He/Him",
+  "Age" => 27,
+  "Height in cm" => 193.04,
+  "Is Exited" => true,
+  "Works a lot" => true,
+  "Programming lanuages" => ["ruby", "java", "MatLab"],
+  "Past Jobs" => [
+    "tutor", "dishwasher", "scooper at b&j", "barista", "supervisor"
+  ]
+}
