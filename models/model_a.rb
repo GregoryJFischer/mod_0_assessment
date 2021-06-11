0f6177c823ce00c6ed59636113563b775f39bc75
Gregory Fischer <gwhoisj@gmail.com>
2021-06-10 22:41:32 -0400

    Add T Rex

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+tyrannosaurus_rex = {
+  "Time period" => "Late Cretaceous",
+  "Diet" => "Carnivore",
+  "Length in Feet" => 40,
+  "Lifespan" => 30,
+  "Is big" => true,
+  "Currently living" => false,
+  "People who like dinosaurs" => ["You", "Me", "Everyone"],
+  "Possible skin types" => ["Hide", "Fur", "Feathers"]
+}
