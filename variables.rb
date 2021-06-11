72edaabe64c45d0a05d1edb9ce599c00e0fc8bea
Gregory Fischer <gwhoisj@gmail.com>
2021-06-10 22:30:15 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Demon Slayer"
+is_entertaining = true
+movie = "Terminator"
+num_4 = 23
+num_6 = 17
+product = num_4 * num_6
+favorite_foods = ["ramen", "steak", "tuna"]
+favorite_foods.shift
+puts movie.length
